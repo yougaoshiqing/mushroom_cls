7728e341e63d8bb1555a983ef8b754331d24ccd6
yougaoshiqing <gaoshiqing2023@gmail.com>
2025-03-08 13:10:36 +0800

    Add files via upload

---
+++ mushrooms_online.py
@@ -0,0 +1,40 @@
+from flask import Flask,request,jsonify
+import pandas as pd 
+import joblib
+import json
+import numpy as np
+
+app = Flask(__name__)
+@app.route("/",methods=['GET','POST'])
+def mushrooms_classifing():
+    if request.method == "POST":
+        model = joblib.load("mushrooms_cls.pkl")
+        pca = joblib.load("pca.pkl")
+        
+        json_data = request.json
+        #print("received data:",json_data)
+        json_dict = json.loads(json_data)
+        #json_int = {k:int(v) for k,v in json_dict.items()}
+        df = pd.DataFrame(json_dict)
+        
+        pca_data = pca.transform(df)
+        prediction = model.predict(pca_data)
+        
+        res_dict = {}
+        y_proba = model.predict_proba(pca_data)
+        for i in range(len(y_proba)):
+            if any(y_proba[i] > 0.8):
+                i_proba = y_proba[i][y_proba[i]>0.8]
+                i_cls = model.classes_[y_proba[i]==i_proba]
+                print(f"{i}th input data's prediction is {i_proba} ,belong to class {i_cls},adding to training")
+                model.partial_fit(np.atleast_2d(pca_data[i]),np.atleast_2d(i_cls))
+                res_dict[i] = {"prediction":prediction[i].item(),"probability":i_proba.item(),"back":"added to training"}
+            else:
+                res_dict[i] = {"prediction":prediction[i].item(),"probability":max(y_proba[i]).item(),"back":"not added to training"}
+        #print("prediction:",prediction)
+        return jsonify(res_dict)
+    else:
+        return jsonify({"waiting":"data"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
